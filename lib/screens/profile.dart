bdb8a26a915bf6f5a2eda58a016d6730ceb9e44f
khoreji <gopalakhoria@gmail.com>
2021-05-04 18:37:23 +0530

    profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -133,7 +133,7 @@ class _ProfileState extends State<Profile> {
           ),
           decoration: BoxDecoration(
               image: DecorationImage(
-                  image: AssetImage("assets/images/bg.jpeg"),
+                  image: AssetImage("assets/images/Hexaleaf.jpg"),
                   fit: BoxFit.fill)),
           height: double.infinity,
           width: double.infinity,
