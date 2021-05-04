66303ea2342c72e7020432aedc83c49548020510
khoreji <gopalakhoria@gmail.com>
2021-05-04 10:44:38 +0530

    version

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MaterialApp(
-    initialRoute: "login",
+    initialRoute: "signup",
     routes: {
       "login": (context) => Login(),
       "signup": (context) => MyReg(),
