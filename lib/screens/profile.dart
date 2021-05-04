550eacbb6d1069da0833ba5e49f0daf4d802f513
bhupendra05 <btale05.bt@gmail.com>
2021-05-04 18:05:24 +0530

    profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -6,8 +8,46 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  static var savedusername = "";
+  static SharedPreferences shrprf;
+  var fsconnect = FirebaseFirestore.instance;
+  var name = "";
+  var email = "";
+  var pno = '';
+  var imgurl = "";
+  var workas = "";
+
+  getdata() async {
+    var data = await fsconnect
+        .collection('TeamProfile')
+        .doc('info')
+        .collection('$savedusername')
+        .doc('$savedusername')
+        .get();
+    setState(() {
+      name = "${data.get('name')}";
+      email = "${data.get('email')}";
+      pno = "${data.get('pno')}";
+      workas = "${data.get('WorkAs')}";
+      imgurl = "${data.get('imgurl')}";
+    });
+  }
+
+  static Future init() async {
+    shrprf = await SharedPreferences.getInstance();
+
+    print("1");
+    savedusername = shrprf.getString("username");
+  }
+
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      init();
+
+      print("$savedusername");
+    });
+    getdata();
     var device = MediaQuery.of(context).size;
     return MaterialApp(
       home: Scaffold(
@@ -16,7 +56,7 @@ class _ProfileState extends State<Profile> {
             children: [
               Positioned(
                   left: device.width * 0.35,
-                  top: device.height * 0.2,
+                  top: device.height * 0.14,
                   width: device.width * 0.3,
                   height: device.width * 0.3,
                   child: Container(
@@ -29,7 +69,74 @@ class _ProfileState extends State<Profile> {
                         Icons.person,
                         size: 70,
                         color: Colors.white,
-                      )))
+                      ))),
+              Positioned(
+                  top: device.height * 0.3,
+                  left: device.width * 0,
+                  width: device.width,
+                  child: Container(
+                    child: Text(
+                      "Profile",
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  )),
+              Positioned(
+                  top: device.height * 0.4,
+                  left: device.width * 0.15,
+                  width: device.width * 0.7,
+                  height: device.height * 0.45,
+                  child: Container(
+                    padding: EdgeInsets.only(left: 12),
+                    decoration: BoxDecoration(
+                        color: Colors.black38,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      //      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "$name",
+                          style: TextStyle(
+                            fontSize: 12,
+                            decoration: TextDecoration.underline,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "$email",
+                          style: TextStyle(
+                            fontSize: 12,
+                            decoration: TextDecoration.underline,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          "$pno",
+                          style: TextStyle(
+                            fontSize: 12,
+                            decoration: TextDecoration.underline,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          "$workas",
+                          style: TextStyle(
+                            fontSize: 12,
+                            decoration: TextDecoration.underline,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  )),
             ],
           ),
           decoration: BoxDecoration(
