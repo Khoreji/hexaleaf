5ea08ea932d2b134a1a197271acedb01f93d4c52
khoreji <gopalakhoria@gmail.com>
2021-05-06 08:41:10 +0530

    gk

--- lib/screens/var.dart
+++ lib/screens/var.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 
-class VarClass extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var device = MediaQuery.of(context).size;
-  }
-}
+// class VarClass extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     var device = MediaQuery.of(context).size;
+//   }
+// }
